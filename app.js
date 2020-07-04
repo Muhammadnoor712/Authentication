93d6ca4d2cbad78a8346df7f68c3402fd720b603
Muhammad Noor <Muhammadnoor712@users.noreply.github.com>
2020-07-05 00:29:17 +0500

    salting and hashing

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const md5 = require("md5");
-
+const bcrypt = require("bcrypt");
+const saltRound = 10;
 const app = express();
 
 app.use(express.static("public"));
@@ -18,7 +18,7 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
   useUnifiedTopology: true
 });
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
@@ -44,35 +44,44 @@ app.get("/register", function(req, res) {
 
 app.post("/register", function(req, res) {
 
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
+  bcrypt.hash(req.body.password, saltRound, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+
   });
-  newUser.save(function(err) {
+
+});
+
+app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
     if (err) {
       console.log(err);
     } else {
-      res.render("secrets");
-    }
-  });
-});
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+  if(result===true){
+  res.render("secrets");
 
-app.post("/login" , function(req , res){
-const username = req.body.username;
-const password = md5(req.body.password);
-
-User.findOne({email:username} , function(err ,foundUser){
-  if(err){
-    console.log(err);
-  }else {
-    if(foundUser){
-      if (foundUser.password===password) {
-        res.render("secrets");
-
-      }
-    }
   }
 });
+                
+      }
+    }
+  });
 
 });
 
